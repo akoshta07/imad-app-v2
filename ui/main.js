f53f572726f1c94ae4436da17455f766c25476c7
akoshta07 <akoshta18@gmail.com>
2017-02-04 12:49:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element=document.getElementById('main-text');
 
 element.innerHTML='New value';
 //move the image
-var element=document.getElementById('madi');
+var img=document.getElementById('madi');
  img.onclick = function(){
      img.style.marginleft='100px'; 
  };   
