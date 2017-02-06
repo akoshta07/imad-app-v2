78c088ecafd2afdc7ac15770811dc91f8c94d24a
akoshta07 <akoshta18@gmail.com>
2017-02-06 12:30:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,14 @@ button.onclick = function(){
     request.open('GET','http://akoshta07.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit =documnet.getElementById('submit_btn');
+Submit.onclick = function(){
+    //Maki a request to the server and sent the name
+    
+    //Capture a list of name and render it as a list
+    var name =['name1','name2','naeme'];
+    var list='';
+}
