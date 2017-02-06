136033384d90e06408272e3c330178086978e593
akoshta07 <akoshta18@gmail.com>
2017-02-06 20:17:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,6 +32,11 @@ Submit.onclick = function(){
     //Maki a request to the server and sent the name
     
     //Capture a list of name and render it as a list
-    var name =['name1','name2','naeme'];
+    var names =['name1','name2','name3','name4'];
     var list='';
-}
+    for(var i=0;i<names.length;i++){
+        list += '<li>' + name[i] +'</li>';
+    }
+    var ul=document.getElemnetById('namelist');
+    ul.innerHTML = list;
+};
