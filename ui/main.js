e9ceeb859507008f71bd62f71b6bb43bf55eb53e
akoshta07 <akoshta18@gmail.com>
2017-02-07 21:38:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,7 +53,7 @@ submit.onclick = function(){
    //Make the request
    var nameInput = document.getElementById('name');
    var name = nameInput.value;
-    request.open('GET','http://akoshta07.imad.hasura-app.io/sumbit-name?name=' + name,true);
+    request.open('GET','http://akoshta07.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
     
 };
