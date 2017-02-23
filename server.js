67f9252ea99df3bd162d30f2d9d50487e8c13cdc
akoshta07 <akoshta18@gmail.com>
2017-02-23 11:51:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,12 +88,12 @@ app.get('/', function (req, res) {
 });
 
 var pool = new Pool(config);
-app.get('/test-db',function(req,res){
-    pool.query('SELECT * FROM text',function(err,result){
+app.get('/test-db',function(req,res){ 
+    pool.query('SELECT * FROM test',function(err,result){
           if(err){ 
               res.status(500).send(err.toString());
-          } else{
-              res.send(JASON.stringfy(result));
+          } else{ 
+              res.send(JSON.stringify(result));
           }
     });
 });
