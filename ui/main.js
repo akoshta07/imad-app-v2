1f116b5db926f11611d4abd083aaf425e006f178
akoshta07 <akoshta18@gmail.com>
2017-03-08 11:01:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,11 +24,11 @@ submit.onclick = function(){
     };
    //Make the request
    var username = document.getElementById('username').value;
-   var password=document.getElementById('password').value;
+   var password = document.getElementById('password').value;
    console.log(username);
    console.log(password);
     request.open('POST','http://akoshta07.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({username:"username",password:"password"}));
+    request.send(JSON.stringify({username:username,password:password}));
     
 };
